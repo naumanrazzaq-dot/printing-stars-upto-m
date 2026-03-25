989f402768b1d05988ab86c29049f81bdec4e53e
Muhammad Nauman <maliknomigep@gmail.com>
2026-03-25 11:54:45 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout <<"enter lines number";
+	cin>>n;
+    for(int i=1; i<=n; i++){ //For star lines that how much we lines want
+        int m=50;
+            for(int i=1; i<=m; i++){ // this is for what we want to print
+                cout<<"*";
+
+
+            }
+             cout<<endl;
+    }
+
+}
